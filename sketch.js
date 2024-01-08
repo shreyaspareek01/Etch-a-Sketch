64bc9374ddaed32ec4591f8c8b03cecb2a506477
Shreyas Pareek <shreyaspareek3101@gmail.com>
2024-01-08 16:41:44 +0530

    Update 3 files
    Added color input, clear button and an eraser button,changed styling

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
 const body = document.querySelector(".body");
 const container = document.createElement("div");
 container.classList.add("container");
-let isClicking = false;
 
+let colorPicker = document.getElementById("colorPicker");
+colorPicker.value = "#ff5050";
+let selectedColor = "#ff5050";
+
+let sqPerSide = prompt("Enter the squares per side");
+
+colorPicker.addEventListener("input", (e) => {
+  selectedColor = e.target.value;
+});
+
+let isClicking = false;
 container.addEventListener("mousedown", () => {
   isClicking = true;
 });
@@ -10,14 +20,36 @@ container.addEventListener("mousedown", () => {
 container.addEventListener("mouseup", () => {
   isClicking = false;
 });
+
 for (let i = 1; i <= 256; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
   square.addEventListener("mouseover", () => {
     if (isClicking) {
-      square.style.backgroundColor = "black";
+      square.style.backgroundColor = selectedColor;
     }
   });
   container.appendChild(square);
 }
+container.style["border"] = "20px solid rgb(222, 80, 80)";
+container.style["border-radius"] = "20px";
 body.appendChild(container);
+
+let clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
+
+eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", () => {
+  lastColor = selectedColor;
+  selectedColor = "white";
+});
+
+btn1 = document.querySelector(".btn1");
+btn1.addEventListener("click", () => {
+  selectedColor = lastColor;
+});
